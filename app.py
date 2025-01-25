6df548152513a8b24b63430fbc94d43f4dd907f3
Andy Barajas <46165581+barajas123@users.noreply.github.com>
2025-01-25 12:39:40 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """Returns the difference of two numbers."""
+    return a - b
+
+if __name__ == "__main__":
+    print("Addition: ", add(2, 3))
+    print("Subtraction: ", subtract(5, 3))
